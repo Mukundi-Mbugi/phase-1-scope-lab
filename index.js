c29c7905c27f0d5981ef504d8ed8f8abafac913e
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-11 11:31:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName='bob'
+var bestCustomer;
+const leastFavoriteCustomer='jade'
+// var bestCustomer='not bob'
+
+function upperCaseCustomerName(){
+   customerName=customerName.toUpperCase()
+}
+
+    // var bestCustomer='not bob'
+  function setBestCustomer(){
+      bestCustomer='not bob'
+  }  
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+// 
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Koki'
+}
+console.log(bestCustomer);
